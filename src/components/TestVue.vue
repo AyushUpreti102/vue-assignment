983a22c00859f8002e5cd46121d7fa64be7881dd
AyushUpreti102 <ayushupreti102@gmail.com>
2022-10-06 16:36:25 +0530

    feat/Assignement 1

---
+++ src/components/TestVue.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="TestVue">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, aliquam.</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TestVue',
+    destroyed() {
+        console.warn("TestVue destroyed")
+    },
+    created() {
+        console.warn("TestVue Created")
+    },
+    mounted() {
+        console.warn("TestVue Mounted")
+    }
+}
+</script>
+  
