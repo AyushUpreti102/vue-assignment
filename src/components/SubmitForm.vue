788b55a7190dbb57d9d820a10331ed4551911f2f
AyushUpreti102 <ayushupreti102@gmail.com>
2022-10-10 17:10:55 +0530

    form assingment by different components

---
+++ src/components/SubmitForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="submit-form">
+        <button @click="saveUserDetails">Submit</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SubmitForm',
+    props: ['enteredDetails', 'storedDetails'],
+    methods: {
+        saveUserDetails() {
+            if (this.enteredDetails.name === '' || this.enteredDetails.email === '' || this.enteredDetails.phno === '') {
+                alert('please fill the form');
+            }
+            else {
+                this.$emit('savingDetails', this.enteredDetails);
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
