2af5a53f17249c978feb7a95485e4856e2b731bf
AyushUpreti102 <ayushupreti102@gmail.com>
2022-10-13 19:07:36 +0530

    created timer

---
+++ src/components/TimeControler.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="TimeControler">
+        <button @click="startTimer">start</button>
+        <button @click="stopTimer">stop</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TimeControler',
+    methods: {
+        startTimer() {
+            this.$emit('startTimer', true);
+        },
+        stopTimer() {
+            this.$emit('stopTimer', true);
+        }
+    }
+}
+</script>
+<style scoped>
+.TimeControler button {
+    padding: 10px;
+    min-width: 100px;
+    background-color: greenyellow;
+    border: none;
+    border-radius: 5px;
+    margin: 0 10px;
+}
+</style>
