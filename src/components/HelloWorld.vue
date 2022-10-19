e4390fd56f419eb63c9eb592ee7da437f8555ab6
AyushUpreti102 <ayushupreti102@gmail.com>
2022-10-19 11:24:26 +0530

    pagination assingment

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,54 +1,110 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    <ul class="hello__List">
+      <li v-for="(value, index) in paginated" :key="index">{{value.name}}</li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="hello__buttons">
+      <button @click="previousPage" :disabled="state2">previous</button>
+      <button v-for="(value, index) in paginated" :key="index" @click="changepage(index)">{{index+1}}</button>
+      <button @click="nextPage" :disabled="state1">next</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      users: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }, { name: 'item4' }, { name: 'item5' },
+      { name: 'item6' }, { name: 'item7' }, { name: 'item8' }, { name: 'item9' }, { name: 'item10' }, { name: 'item11' },
+      { name: 'item12' }, { name: 'item13' }, { name: 'item14' }, { name: 'item15' }, { name: 'item16' }, { name: 'item17' },
+      { name: 'item18' }, { name: 'item19' }, { name: 'item20' }, { name: 'item21' }, { name: 'item22' }, { name: 'item23' },
+      { name: 'item24' }, { name: 'item25' }],
+      itemPerPage: 5,
+      currentPage: 2,
+      pageCount: 5,
+      pageItems: 5,
+      paginated: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }, { name: 'item4' }, { name: 'item5' }],
+      state1: false,
+      state2: true,
+      page: 0,
+    }
+  },
+  methods: {
+    nextPage() {
+      this.page = this.currentPage - 1;
+      this.pageItems = this.pageItems + this.itemPerPage;
+      this.paginated = this.users.slice(this.page * this.itemPerPage, this.pageItems);
+      this.currentPage++;
+      if (this.pageItems === 25) {
+        this.state1 = true;
+      }
+      if (this.pageItems > this.itemPerPage) {
+        this.state2 = false;
+      }
+    },
+    previousPage() {
+      this.currentPage--;
+      this.page = this.currentPage - 2;
+      this.pageItems = this.pageItems - this.itemPerPage;
+      this.paginated = this.users.slice(this.page * this.itemPerPage, this.pageItems);
+      if (this.pageItems < 25) {
+        this.state1 = false
+      }
+      if (this.pageItems === this.itemPerPage) {
+        this.state2 = true;
+      }
+    },
+    changepage(index) {
+      this.currentPage = (index + 1) + 1;
+      this.page = index;
+      this.pageItems = this.itemPerPage * (index + 1)
+      this.paginated = this.users.slice(this.page * this.itemPerPage, this.pageItems);
+      if (this.pageItems === 25) {
+        this.state1 = true;
+      }
+      if (this.pageItems > this.itemPerPage) {
+        this.state2 = false;
+      }
+      if (this.pageItems < 25) {
+        this.state1 = false
+      }
+      if (this.pageItems === this.itemPerPage) {
+        this.state2 = true;
+      }
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.hello__List {
+  width: 50%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.hello__List li {
+  width: 100%;
+  border: 1px solid black;
+  list-style: none;
+  padding: 10px;
+  background-color: lightgrey;
 }
-a {
-  color: #42b983;
+
+.hello__buttons {
+  width: 50%;
+  display: flex;
+  position: relative;
+  left: 30px;
+}
+
+button {
+  width: 20%;
+  padding: 10px;
+  margin: 5px;
 }
 </style>
