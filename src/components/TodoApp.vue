bc8e63941c42233ab4d9699f0eeb060fc835a827
AyushUpreti102 <ayushupreti102@gmail.com>
2022-10-21 17:55:07 +0530

    made form using vuex

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <v-text-field type="text" label="Task" v-model="msg"></v-text-field>
+        <v-btn @click.prevent="addTask">Add</v-btn>
+    </v-container>
+</template>
+<script>
+export default {
+    name: "TodoApp",
+    data() {
+        return {
+            msg: ''
+        }
+    },
+    computed: {
+        newTask() {
+            return this.$store.getters.newTask;
+        },
+    },
+    methods: {
+        addTask() {
+            this.$store.commit('addTask', this.msg)
+            this.msg = ''
+        },
+    }
+}
+</script>
